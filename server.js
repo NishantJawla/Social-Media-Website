966892b7922143e865ef5ddf34cb1f1fc1535d59
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-03-28 20:00:09 +0530

    Added a connection to the mongodb

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
+//importing keys
+const db = require('./config/keys').mongoURI;
+//connect to mongoDB
+mongoose
+.connect(db,{useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> console.log('DB succesfully connected')).
+catch(err => console.log(err))
 app.get('/', (req, res) => res.send('Hello There!'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=> console.log(`application will be running at port ${PORT}`));
